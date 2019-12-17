d609f4ce5e3e1677e28e387ab5352419a07f0bfc
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2019-12-17 23:43:02 +0530

    colorchanges

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,11 +2,14 @@ import React from 'react';
 class Services extends React.Component{
     render(){
         return(
-            <div>
-                <div className="services" id="one"></div>
-                <div className="services" id="two"></div>
-                <div className="services" id="three"></div>
-                <div className="services" id="four"></div>
+            <div className="imageholder">
+                <div className="services" id="one">
+                    <h1 className="text">Household Staffing</h1>
+                    <h2 className="text1">Heloooooooooooooooo</h2>
+                </div>
+                <div className="services" id="two"><h1 className="text">Cleaning</h1></div>
+                <div className="services" id="three"><h1 className="text">Office Staffing</h1></div>
+                <div className="services" id="four"><h1 className="text">HouseholdService</h1></div>
             </div>
         )
     }
